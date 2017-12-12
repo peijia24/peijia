453b6b9197270bf1add014e6e1c7180e7390ad10
peijiading <peijia.dng@gmail.com>
2017-12-12 14:08:13 -0800

    inserted memphis img for portfolio

--- scripts.js
+++ scripts.js
@@ -70,7 +70,7 @@ $('#c').mousedown(function() {
 var clearBtn = document.getElementById('drawBtn');
 
 clearBtn.addEventListener('click',function(){
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.reset(0, 0, canvas.width, canvas.height);
 });
 
 
