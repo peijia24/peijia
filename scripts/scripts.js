a75f59982774424a5fb6e25f6a6a801a805f4b98
peijiading <peijia.dng@gmail.com>
2018-01-06 16:24:46 -0800

    added responsive scroll nav

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,77 +1,38 @@
-// Javascript Document for Portfolio Page //
+// Disappearing nav bar on scroll up and down //
 
-const $projectLink = $('.project-link');
-const $projectBoxes = $('.section-box');
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
 
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
 
-$projectLink.click(function(e){
- 
-  e.preventDefault();
- 
-  const theID = $(this).attr('href');
-  
-  const $projectBox = $(theID);
-  
-  console.log($projectBoxes);
-
-  $projectBoxes.css('z-index', '-1');  
-
-  $projectBox.css('z-index', '2');
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
    
-});
-
-
-//Javascript for free draw
-
-var canvas = document.getElementById('c');
-var context = canvas.getContext('2d');
-
-//colors
-window.color = '#fff';
-function rainbows() {
-    return hue = 'rgb('
-        + (Math.floor(Math.random() * 256)) + ','
-        + (Math.floor(Math.random() * 256)) + ','
-        + (Math.floor(Math.random() * 256)) + ')';
-}
-
-$('select').change(function() {
-    if( $('select option:selected').attr('value') != 'rainbow') {
-        window.color = $('select option:selected').attr('value');
-    }else{window.color = rainbows();}
-});
-
-
-//Freehand Draw
-var started = false;
-
-$('#c').mousedown(function() {
-    context.beginPath();
-    started = true;
-}).mousemove(function(f) {
-    if(started == true){
-        if( $('select option:selected').attr('value') == 'rainbow' ){
-            window.color = rainbows();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+   
+    if (st > lastScrollTop && st > navbarHeight){
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
         }
-        var offSet = $('#c').offset();
-        var endx = f.pageX - offSet.left;
-        var endy = f.pageY - offSet.top;
-        context.lineTo(endx,endy);
-        context.strokeStyle = color;
-        context.stroke();
-    };
-}).mouseup(function() {
-    started = false; 
-}).mouseleave(function() {
-   started = false;
-});
+    }
+    
+    lastScrollTop = st;
 
-var clearBtn = document.getElementById('drawBtn');
-
-clearBtn.addEventListener('click',function(){
-  context.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-
+    // console.log("scrolling");
+}
 
